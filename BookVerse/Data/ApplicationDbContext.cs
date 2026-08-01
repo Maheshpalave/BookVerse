6baeb199382c08cdae0558893846e080c97caf9c
Dipak Palve <DELL@DESKTOP-JFJM8FG>
2026-08-02 02:08:13 +0530

    Setup Entity Framework Core and ApplicationDbContext

---
+++ BookVerse/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookVerse.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BookVerse.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
